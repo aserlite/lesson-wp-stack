8bebe6506de036e0b1a6df6a0ad9cfd6b47a7bce
aserlite <arthurcuvillon691@gmail.com>
2024-01-25 10:24:10 +0100

    add first info plugin

---
+++ htdocs/wp-content/plugins/Arthur/Arthur.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Arthur
+Plugin URI: http://wordpress.org/plugins/hello-dolly/
+Description: Il est super cool ce plugin
+Author: Arthur
+Version: 1.0.0
+Author URI: http://ma.tt/
+*/
+
